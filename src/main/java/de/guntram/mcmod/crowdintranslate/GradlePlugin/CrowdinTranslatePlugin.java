20de089088e75499aafc76452ccdd1995cd9bb6d
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2024-03-01 20:39:18 -0800

    made DownloadTask use task inputs instead of static extension instance

--- src/main/java/de/guntram/mcmod/crowdintranslate/GradlePlugin/CrowdinTranslatePlugin.java
+++ src/main/java/de/guntram/mcmod/crowdintranslate/GradlePlugin/CrowdinTranslatePlugin.java
@@ -7,20 +7,25 @@ package de.guntram.mcmod.crowdintranslate.GradlePlugin;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
 
 /**
  *
  * @author gbl
  */
 public class CrowdinTranslatePlugin implements Plugin<Project> {
-
-    public static CrowdinTranslateParameters parameters;
-
     @Override
     public void apply(Project project) {
-        
-        parameters = project.getExtensions()
+        final CrowdinTranslateParameters parameters = project.getExtensions()
                 .create("crowdintranslate", CrowdinTranslateParameters.class);
-        project.getTasks().create("downloadTranslations", DownloadTask.class);
+        parameters.getVerbose().convention(false);
+
+        project.getTasks().register("downloadTranslations", DownloadTask.class).configure(task -> {
+            task.getCrowdinProjectName().set(parameters.getCrowdinProjectName());
+            task.getMinecraftProjectName().set(parameters.getMinecraftProjectName());
+            task.getJsonSourceName().set(parameters.getJsonSourceName());
+            task.getVerbose().set(parameters.getVerbose());
+        });
     }
 }
--- src/main/java/de/guntram/mcmod/crowdintranslate/GradlePlugin/DownloadTask.java
+++ src/main/java/de/guntram/mcmod/crowdintranslate/GradlePlugin/DownloadTask.java
@@ -2,26 +2,41 @@ package de.guntram.mcmod.crowdintranslate.GradlePlugin;
 
 import de.guntram.mcmod.crowdintranslate.CrowdinTranslate;
 import org.gradle.api.DefaultTask;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.TaskAction;
 
-public class DownloadTask extends DefaultTask {
+public abstract class DownloadTask extends DefaultTask {
+    @Input
+    @Optional
+    public abstract Property<String> getCrowdinProjectName();
+
+    @Input
+    public abstract Property<String> getMinecraftProjectName();
+
+    @Input
+    public abstract Property<String> getJsonSourceName();
+
+    @Input
+    public abstract Property<Boolean> getVerbose();
+
     @TaskAction
     public void action() {
-        CrowdinTranslateParameters parms = CrowdinTranslatePlugin.parameters;
-        if (parms.getCrowdinProjectName() == null) {
+        if (!getCrowdinProjectName().isPresent()) {
             System.err.println("No crowdin project name given, nothing downloaded");
             return;
         }
-        String[] args = new String[ (parms.getVerbose().get() ? 4 : 3) ];
+        String[] args = new String[ (getVerbose().get() ? 4 : 3) ];
         int argc = 0;
-        if (parms.getVerbose().get()) {
+        if (getVerbose().get()) {
             args[argc++] = "-v";
         }
-        String cpn = parms.getCrowdinProjectName().get();
-        String mpn = parms.getMinecraftProjectName().get();
+        String cpn = getCrowdinProjectName().get();
+        String mpn = getMinecraftProjectName().get();
         args[argc++] = cpn;
         args[argc++] = (mpn == null ? cpn : mpn);
-        args[argc++] = parms.getJsonSourceName().get();
+        args[argc++] = getJsonSourceName().get();
 
         CrowdinTranslate.main(args);
         this.setDidWork(true);
